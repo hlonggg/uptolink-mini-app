9d1a955d96bab95e00dd8e0cdc2acb10ad043029
hlonggg <139870340+hlonggg@users.noreply.github.com>
2026-07-15 16:22:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ from monitor import (
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8983631020:AAHitwdCI9SyIeTqR2Ukr50Ng_V84JmcE7U")
 BASE_URL = os.getenv("BASE_URL", "https://your-project.railway.app")
 
-app = Flask(__name__, static_folder='../frontend', static_url_path='')
+# Thay đổi đường dẫn static
+app = Flask(__name__, static_folder='frontend', static_url_path='')
 CORS(app)
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
